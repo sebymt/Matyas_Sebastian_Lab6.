af32555efe0297033739f20740159ace40b123e8
SEB <SEB@VERSACE>
2022-01-02 05:42:31 +0200

    Lab 11

---
+++ Controllers/CustomersGrpcController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcCustomersService;
+using Matyas_Sebastian_Lab2.Models;
+
+namespace Matyas_Sebastian_Lab2.Controllers
+{
+    public class CustomersGrpcController : Controller
+    {
+        private readonly GrpcChannel channel;
+        public CustomersGrpcController()
+        {
+            channel = GrpcChannel.ForAddress("https://localhost:5001");
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var client = new CustomerService.CustomerServiceClient(channel);
+            CustomerList cust = client.GetAll(new Empty());
+            return View(cust);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(GrpcCustomersService.Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = new
+                CustomerService.CustomerServiceClient(channel);
+                var createdCustomer = client.Insert(customer);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+    }
+}
